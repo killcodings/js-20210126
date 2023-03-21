fe027052ce0a878c98f06d490e36f0666924ca72
killcodings <kontaktid@ya.ru>
2023-03-21 02:27:36 +0000

    feat: 6 tasks

---
+++ 06-events-practice/2-tooltip/index.js
@@ -0,0 +1,83 @@
+class Tooltip {
+  static instance;
+
+  element;
+
+  onMouseOver = event => {
+    const element = event.target.closest('[data-tooltip]');
+
+    if (element) {
+      this.render(element.dataset.tooltip);
+      this.moveTooltip(event);
+
+      document.addEventListener('pointermove', this.onMouseMove);
+    }
+  };
+
+  onMouseMove = event => {
+    this.moveTooltip(event);
+  };
+
+  onMouseOut = () => {
+    this.removeTooltip();
+  };
+
+  removeTooltip() {
+    if (this.element) {
+      this.element.remove();
+      this.element = null;
+
+      document.removeEventListener('pointermove', this.onMouseMove);
+    }
+  }
+
+  constructor() {
+    if (Tooltip.instance) {
+      return Tooltip.instance;
+    }
+
+    Tooltip.instance = this;
+  }
+
+  initEventListeners() {
+    document.addEventListener('pointerover', this.onMouseOver); // над
+    document.addEventListener('pointerout', this.onMouseOut); // за
+  }
+
+  initialize() {
+    this.initEventListeners();
+  }
+
+  render(html) {
+    this.element = document.createElement('div');
+    this.element.className = 'tooltip';
+    this.element.innerHTML = html;
+
+    document.body.append(this.element);
+  }
+
+  moveTooltip(event) {
+    const shift = 20;
+    const left = event.clientX + shift;
+    const top = event.clientY + shift;
+
+    // TODO: Add logic for window borders
+
+    this.element.style.left = `${left}px`;
+    this.element.style.top = `${top}px`;
+  }
+
+  remove () {
+    this.element.remove();
+  }
+
+  destroy() {
+    document.removeEventListener('pointerover', this.onMouseOver);
+    document.removeEventListener('pointerout', this.onMouseOut);
+    this.removeTooltip();
+  }
+}
+
+const tooltip = new Tooltip();
+
+export default tooltip;
